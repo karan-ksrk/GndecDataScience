b905ba57afdb761d0ce31effa6e20b0677d31a2c
karan-ksrk <44173143+karan-ksrk@users.noreply.github.com>
2020-10-21 11:55:04 +0530

    adding into database

---
+++ question/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class QuestionForm(forms.Form):
+    question = forms.CharField(widget=forms.Textarea)
+    option1 = forms.CharField(widget=forms.Textarea)
+    option2 = forms.CharField(widget=forms.Textarea)
+    option3 = forms.CharField(widget=forms.Textarea)
+    option4 = forms.CharField(widget=forms.Textarea)
+    correct = forms.CharField()
--- question/views.py
+++ question/views.py
@@ -1,9 +1,22 @@
-from django.http  import HttpResponse
+from django.http  import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Question
+from .forms import QuestionForm
+
 
 def question_list(request):
     return HttpResponse('Home View')
 
 def question_create(request, *args, **kwargs):
+  
+    if request.method == "POST":
+        post_data = request.POST or None
+        if post_data != None:
+            print(request.POST)
+            form = QuestionForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                ques_object = Question.objects.create(**data)
+                form = QuestionForm()
+                ques_object.save()
     return render(request, 'question/form.html', {})
